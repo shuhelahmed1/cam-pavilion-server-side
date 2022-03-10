1a6c0e0376b689d6a74f95aa5c3f23cd8f169624
shuhelahm <shuhel037@gmail.com>
2022-03-10 12:07:16 +0600

    api created for delete orders and get orders api modified

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oesrn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -65,9 +65,23 @@ async function run() {
 
     // get api for orders
     app.get('/orders', async(req,res)=>{
-      const cursor = ordersCollection.find({});
+      let query = {};
+      const email = req.query.email;
+      if(email){
+        query = {email: email}
+      }
+      const cursor = ordersCollection.find(query);
       const result = await cursor.toArray();
       res.send(result)
+    })
+
+    // delete api for orders
+    app.delete('/orders/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const result = await ordersCollection.deleteOne(query)
+      console.log(result)
+      res.json(result)
     })
   } finally {
     // await client.close();
