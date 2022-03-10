7096f0eb20195ec07c0fd32cac52f71f166d2856
shuhelahm <shuhel037@gmail.com>
2022-03-10 18:44:56 +0600

    put api created for users

--- index.js
+++ index.js
@@ -99,7 +99,16 @@ async function run() {
       const user = req.body;
       const result = await usersCollection.insertOne(user)
       res.json(result)
-      console.log(result)
+    })
+
+    // put api for users
+    app.put('/users', async(req,res)=>{
+      const user = req.body;
+      const filter = {email: user.email};
+      const options = {upsert: true};
+      const updateDoc = {$set: user};
+      const result = await usersCollection.updateOne(filter,updateDoc,options);
+      res.json(result);
     })
 
   } finally {
