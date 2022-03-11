6d8b9e8c425adf9ff4dc7aa861ce751429ede88f
shuhelahm <shuhel037@gmail.com>
2022-03-11 11:00:27 +0600

    modified

--- index.js
+++ index.js
@@ -124,7 +124,7 @@ async function run() {
     app.get('/users/:email', async(req,res)=>{
       const email = req.params.email;
       const query = {email: email};
-      const user = await usersCollection.findOne(email);
+      const user = await usersCollection.findOne(query);
       let isAdmin = false;
       if(user?.role === 'admin'){
         isAdmin(true)
