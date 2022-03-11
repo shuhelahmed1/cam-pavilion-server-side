6cda234c4509b25259eabf8aea449456c054fcab
shuhelahm <shuhel037@gmail.com>
2022-03-11 08:20:26 +0600

    put api created for making an user admin

--- index.js
+++ index.js
@@ -111,6 +111,15 @@ async function run() {
       res.json(result);
     })
 
+    // put api for users admin
+    app.put('/users/admin', async(req,res)=>{
+      const user = req.body;
+      const filter = {email: user.email}
+      const updateDoc = {$set: {role: 'admin'}}
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.json(result)
+    })
+
   } finally {
     // await client.close();
   }
