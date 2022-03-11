afb8be14248fab726ac0e16c4e8cb1c4f7d76f3c
shuhelahm <shuhel037@gmail.com>
2022-03-11 11:23:19 +0600

    updated

--- index.js
+++ index.js
@@ -121,16 +121,16 @@ async function run() {
     })
 
     // get api for specific user
-    // app.get('/users/:email', async(req,res)=>{
-    //   const email = req.params.email;
-    //   const query = {email: email};
-    //   const user = await usersCollection.findOne(query);
-    //   let isAdmin = false;
-    //   if(user?.role === 'admin'){
-    //     isAdmin=true;
-    //   }
-    //   res.json({admin: isAdmin})
-    // })
+    app.get('/users/:email', async(req,res)=>{
+      const email = req.params.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if(user?.role === 'admin'){
+        isAdmin=true;
+      }
+      res.json({admin: isAdmin})
+    })
 
   } finally {
     // await client.close();
