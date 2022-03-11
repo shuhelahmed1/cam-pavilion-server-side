648cb1250e759746ded0155d271b5d2dfa8b4888
shuhelahm <shuhel037@gmail.com>
2022-03-11 11:06:10 +0600

    updated

--- index.js
+++ index.js
@@ -127,7 +127,7 @@ async function run() {
       const user = await usersCollection.findOne(query);
       let isAdmin = false;
       if(user?.role === 'admin'){
-        isAdmin(true)
+        isAdmin=true;
       }
       res.json({admin: isAdmin})
     })
